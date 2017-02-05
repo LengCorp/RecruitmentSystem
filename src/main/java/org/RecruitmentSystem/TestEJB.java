6d9d867465e103a5ef4ff3fb7a17ac78237eda3d
Emil Lengman <emillen@kth.se>
2017-02-05 17:23:15 +0100

    testing stuff

---
+++ src/main/java/org/RecruitmentSystem/TestEJB.java
@@ -0,0 +1,17 @@
+package org.RecruitmentSystem;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Created by daseel on 2/5/17.
+ */
+@Stateless
+@LocalBean
+public class TestEJB {
+
+    public String bajs() {
+
+        return "bajs";
+    }
+}
---
+++ src/main/java/org/RecruitmentSystem/TestFront.java
@@ -0,0 +1,35 @@
+package org.RecruitmentSystem;
+
+import javax.ejb.EJB;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Created by daseel on 2/5/17.
+ */
+@Named("testFront")
+@SessionScoped
+public class TestFront {
+
+    private String hue;
+
+    @EJB
+    TestEJB ejb;
+
+    public TestFront(){
+        hue = "hue";
+    }
+
+    public String getHue() {
+
+        return hue;
+    }
+
+    public void setHue(String s) {
+        // hue
+    }
+
+    public String getBajs(){
+        return ejb.bajs();
+    }
+}
