ed44bf6e21c54c75c8688f1fc2d178ebeeb9f11b
Emil Lengman <emillen@kth.se>
2017-02-22 15:30:16 +0100

    jpa now working

--- src/main/java/org/recruitmentSystem/integration/PersonDAO.java
+++ src/main/java/org/recruitmentSystem/integration/PersonDAO.java
@@ -2,26 +2,29 @@ package org.recruitmentSystem.integration;
 
 import org.recruitmentSystem.model.Person;
 
-import javax.persistence.*;
-import javax.transaction.Transactional;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created by Hiden on 2/9/2017.
  */
-@Transactional
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+@Stateless
 public class PersonDAO {
 
-    EntityManager em;
+    @PersistenceContext(unitName = "RSPU")
+    private EntityManager em;
+
     public PersonDAO(){
-        super();
-        em = Persistence.createEntityManagerFactory("RSPU").createEntityManager();
     }
 
-    public String addUser( int personId, String name, String surname, String ssn, String email, String password, int roleId, String username){
+    public String addUser(String name, String surname, String ssn, String email, String password, int roleId, String username){
 
         Person person = new Person();
 
-        person.setPersonId(personId);
         person.setName(name);
         person.setSurname(surname);
         person.setSsn(ssn);
@@ -30,14 +33,12 @@ public class PersonDAO {
         person.setUsername(username);
         person.setRoleId(roleId);
         try{
-        em.persist(person);
+            em.persist(person);
         }catch (Exception ex){
             ex.printStackTrace();
             return "fail";
         }
-        em.close();
         System.out.println("success");
         return "success";
     }
-
 }
